5590be080f236074974e6453903bf2cb74e2d1d0
nosirbek030381 <ismoilovn899@gmail.com>
2024-08-14 18:39:35 +0500

    crypto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SingleCryptoPage from './app/crypto';
+import Home from './app/page';
+import Header from './components/header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<>
+			<Header />
+
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/crypto/:id' element={<SingleCryptoPage />} />
+			</Routes>
+		</>
+	);
+};
 
 export default App;
